e0cf9d7cd029427faa357eb17cb8388f0e526c41
Falcao-ne-fi <henrique.m.falcao67@gmail.com>
2026-04-30 09:12:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function somar(){
+    var num1 = document.getElementById("n1").valueAsNumber
+    var num2 = document.getElementById("n2").valueAsNumber
+    var res = document.getElementById("res")
+    res.textContent = num1 + num2
+}
