d9c28bdd8240418cc2b6b89e9a866a4d8f647310
Falcao <henrique.m.falcao67@gmail.com>
2026-05-05 08:24:14 -0300

    Add files via upload

---
+++ calculoMedia/script.js
@@ -0,0 +1,14 @@
+function calcularMedia(){
+    var nota1 = parseFloat(document.getElementById('n1').value)
+    var nota2 = parseFloat(document.getElementById('n2').value)
+    var nota3 = parseFloat(document.getElementById('n3').value)
+    var resp = document.getElementById('resposta')
+    var media = (nota1 + nota2 + nota3) / 3
+
+    if(media >= 7){
+        resp.textContent = `${media.toFixed(2)} - PASSOU DE ANO😘`
+    }
+    else{
+        resp.textContent = `${media.toFixed(2)} - REPROVOU DE ANO😎`  
+    }
+}
